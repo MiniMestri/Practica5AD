f6e2b4e0f33fb94a49e4bebd7f329c144329783d
MiniMestri <fonsild97@hotmail.com>
2023-11-30 20:46:06 +0100

    Conector nuevo
    He reutilizado mi codigo de la práctica 3 para que el conector que desarrolle tenga las funciones de insertar,eliminar, buscar y crear archivo

--- Conector.py
+++ Conector.py
@@ -4,16 +4,16 @@ class Jvdb:
     def __init__(self,basededatos):
         self.basededatos = basededatos
     def insert(self,grupo,documento,contenido):
-        self.operacion="insert"
+        self.operacion="eliminar"
         self.grupo=grupo
         self.documento=documento
         self.contenido=contenido
-        ruta='"C:\\Users\\fonsi\\Desktop\\ESTUDIO\\IMF 2\\ACCESO A DATOS\\Practicas\\Practica5AD\\jvdb.exe" '+self.operacion+' '+self.basededatos+' '+self.grupo+' '+self.documento+' "'+self.contenido+'"'
+        ruta='"C:\\Users\\fonsi\\Desktop\\ESTUDIO\\IMF 2\\ACCESO A DATOS\\Practicas\\Practica5AD\\codigo.exe" '+self.operacion+' '+self.basededatos+' '+self.grupo+' '+self.documento+' "'+self.contenido+'"'
         resultado = subprocess.run(ruta,shell=True,stdout=subprocess.PIPE,text=True)
         if resultado.returncode == 0:
             return("ok")
         else:
             return("ERROR")
 
-conexion1= Jvdb("miempresa")
-conexion1.insert("clientes","cliente1","Datos cliente 1")
+conexion1= Jvdb("motogp")
+conexion1.insert("equipos","piloto2","Hola")
